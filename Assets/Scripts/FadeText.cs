b1aa62a221bb788bc54d2a601561fd9e27771c89
Manu <manuelgalvez192@gmail.com>
2024-11-30 14:43:49 +0100

    fade lose and win

---
+++ Assets/Scripts/FadeText.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextSequence : MonoBehaviour
+{
+    public TMP_Text[] texts;
+    public Button finalButton;
+    public float fadeDuration = 1f;
+    public float displayDuration = 2f;
+
+    void Start()
+    {
+        StartCoroutine(ShowTextSequence());
+    }
+
+    IEnumerator ShowTextSequence()
+    {
+        foreach (TMP_Text text in texts)
+        {
+            yield return StartCoroutine(FadeIn(text));
+            yield return new WaitForSeconds(displayDuration);
+            yield return StartCoroutine(FadeOut(text));
+        }
+        finalButton.gameObject.SetActive(true);
+        Debug.Log("Secuencia de textos completada");
+    }
+
+    IEnumerator FadeIn(TMP_Text text)
+    {
+        text.gameObject.SetActive(true);
+        Color color = text.color;
+        color.a = 0f;
+        text.color = color;
+
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            color.a = Mathf.Lerp(0f, 1f, timer / fadeDuration);
+            text.color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        text.color = color;
+    }
+
+    IEnumerator FadeOut(TMP_Text text)
+    {
+        Color color = text.color;
+        color.a = 1f;
+        text.color = color;
+
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            color.a = Mathf.Lerp(1f, 0f, timer / fadeDuration);
+            text.color = color;
+            yield return null;
+        }
+
+        color.a = 0f;
+        text.color = color;
+        text.gameObject.SetActive(false);
+    }
+}
