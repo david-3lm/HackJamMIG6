ee7af336216d3ee85c598e1295280392d5c0e283
Manu <manuelgalvez192@gmail.com>
2024-12-01 01:33:51 +0100

    reloj

---
+++ Assets/Scripts/TimerBar.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerBar : MonoBehaviour
+{
+    public Image fillImage;
+    public float duration = 10;
+
+    private float timeRemaining;
+    private Color startColor = new Color(1f, 1f, 1f, 0.2f);
+    private Color endColor = Color.red;
+    private bool isFlashing = false;
+
+    void Start()
+    {
+        timeRemaining = duration;
+        fillImage.fillAmount = 1;
+        fillImage.color = startColor;
+    }
+
+    public void resetTimer()
+    {
+        timeRemaining = duration;
+        fillImage.fillAmount = 1;
+        fillImage.color = startColor;
+        StopAllCoroutines();
+        isFlashing = false;
+        fillImage.enabled = true;
+    }
+
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            fillImage.fillAmount = timeRemaining / duration;
+
+            fillImage.color = Color.Lerp(startColor, endColor, 1 - fillImage.fillAmount);
+
+            if (timeRemaining <= 4 && !isFlashing)
+            {
+                isFlashing = true;
+                StartCoroutine(FlashFill());
+            }
+        }
+        else
+        {
+            StopAllCoroutines();
+            fillImage.fillAmount = 0;
+        }
+    }
+
+    private IEnumerator FlashFill()
+    {
+        while (true)
+        {
+            fillImage.enabled = !fillImage.enabled;
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+}
